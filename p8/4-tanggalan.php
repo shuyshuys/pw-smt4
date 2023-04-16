36f3cdd4d22365cf5e4674a699d25cf6fcda9a96
shuyshuys <ilvrynsetsuna@protonmail.com>
2023-04-17 01:30:16 +0700

    penyesuaian dan perbaikan module

--- p8/4-tanggalan.php
+++ p8/4-tanggalan.php
@@ -4,15 +4,16 @@
     <title>Tanggalan</title>
 </head>
 <?php
+echo date("m-F-Y, g:i:s a") . "<br>";
 echo "<br>";
 $timezone = date_default_timezone_get();
 echo "Timezone in this server: " . $timezone . "<br>";
-echo date("m-F-Y, g:i:s a") . "<br>"; //membaca tanggal sekarang di server
+echo date("D d-F-Y, g:i:s a") . "<br>"; //membaca tanggal sekarang di server
 echo "<br>";
 date_default_timezone_set('Asia/Jakarta');
 $mytimezone = date_default_timezone_get();
 echo "Timezone in my country: " . $mytimezone . "<br>";
-echo date("m-F-Y, g:i:s a"); //membaca tanggal sekarang di negara saya
+echo date("D d-F-Y, g:i:s a"); //membaca tanggal sekarang di negara saya
 ?>
 
 </html>
