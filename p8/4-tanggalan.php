3e307f61bb1b0522dc1f3efc769032e35663af6d
shuyshuys <ilvrynsetsuna@protonmail.com>
2023-04-16 19:44:26 +0700

    add dif timezone

--- p8/4-tanggalan.php
+++ p8/4-tanggalan.php
@@ -4,9 +4,15 @@
     <title>Tanggalan</title>
 </head>
 <?php
-echo date("m-F-Y, g:i:s a"); //membaca tanggal sekarang
 echo "<br>";
-echo date_default_timezone_get();
+$timezone = date_default_timezone_get();
+echo "Timezone in this server: " . $timezone . "<br>";
+echo date("m-F-Y, g:i:s a") . "<br>"; //membaca tanggal sekarang di server
+echo "<br>";
+date_default_timezone_set('Asia/Jakarta');
+$mytimezone = date_default_timezone_get();
+echo "Timezone in my country: " . $mytimezone . "<br>";
+echo date("m-F-Y, g:i:s a"); //membaca tanggal sekarang di negara saya
 ?>
 
 </html>
