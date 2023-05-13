045aa32134597851f93447de3805ca4e2fc26689
Shuya Setsuna <ilvrynsetsuna@protonmail.com>
2023-05-14 01:42:25 +0700

    add cek koneksi

--- p10/koneksi.php
+++ p10/koneksi.php
@@ -6,3 +6,8 @@ $password   =   "Xii0ChiqoTMvRa2";
 $database   =   "epiz_33929470_latihan";
 // perintah php untuk akses ke database
 $koneksi = mysqli_connect($host, $user, $password, $database);
+
+// cek koneksi
+if (mysqli_connect_errno()) {
+    echo "koneksi database gagal : " . mysqli_connect_error();
+}
